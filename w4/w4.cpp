8d943790af6509cf593b339ffd3c0854d40827d8
HyRoller77 <rharris015@student.butte.edu>
2025-09-16 20:28:42 +0000

    1

---
+++ w4/w4.cpp
@@ -0,0 +1,11 @@
+// ccp code
+#include <cstdio>
+
+int main() {
+    int x = 1;
+    bool b = true;
+    std::printf("%i", b);
+   std::printf("%i", x);
+   std::printf("hello world");
+    return 0;
+}    
